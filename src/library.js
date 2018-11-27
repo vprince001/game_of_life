5ce8da12e17a3763249f17582b29b56ba00ce50e
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-11-27 15:17:31 +0530

    Implement createArray function

---
+++ src/library.js
@@ -0,0 +1,5 @@
+const createArray = function(size) {
+  return new Array(size).fill("D");
+};
+
+module.exports = { createArray };
---
+++ test/libTest.js
@@ -0,0 +1,18 @@
+const assert = require("assert");
+
+const { createArray } = require("../src/library.js");
+
+describe("Create Array ",function() {
+  it("Should return empty array for 0",function() {
+    assert.deepEqual(createArray(0), []);
+  });
+
+  it("Should return one element Array for 1",function() { 
+    assert.deepEqual(createArray(1), ["D"]);
+  });
+
+  it("Should return array of given size",function() { 
+    assert.deepEqual(createArray(5), ["D","D","D","D","D"]);
+  });
+
+});
