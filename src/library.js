160190fdcafd5b91c9e6c5d4b67264f261a5ee9a
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-11-27 15:34:47 +0530

    Add createGrid function

--- src/library.js
+++ src/library.js
@@ -2,4 +2,12 @@ const createArray = function(size) {
   return new Array(size).fill("D");
 };
 
-module.exports = { createArray };
+const createGrid = function(size) {
+  let grid = [];
+  for(let i = 0; i < size; i++) {
+    grid.push(createArray(size));
+  }
+  return grid;
+}
+
+module.exports = { createArray, createGrid };
--- test/libTest.js
+++ test/libTest.js
@@ -1,8 +1,9 @@
 const assert = require("assert");
 
-const { createArray } = require("../src/library.js");
+const { createArray, createGrid } = require("../src/library.js");
 
 describe("Create Array ",function() {
+
   it("Should return empty array for 0",function() {
     assert.deepEqual(createArray(0), []);
   });
@@ -13,6 +14,22 @@ describe("Create Array ",function() {
 
   it("Should return array of given size",function() { 
     assert.deepEqual(createArray(5), ["D","D","D","D","D"]);
+  });
+
+});
+
+describe("Create Grid ",function() {
+
+  it("Should return empty array for 0",function() {
+    assert.deepEqual(createGrid(0),[]);
+  });
+
+  it("Should return one element Array for 1",function() {
+      assert.deepEqual(createGrid(1),[["D"]]);
+  });
+
+  it("Should return array of given size",function() {
+      assert.deepEqual(createGrid(3),[["D","D","D"],["D","D","D"],["D","D","D"]]);
   });
 
 });
